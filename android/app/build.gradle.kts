9c7eae662b6899b5af3f1a64908d554f9a6714a9
yankhofunsani <funsaniyankho@gmail.com>
2025-06-17 04:10:28 +0200

    bulding homepage

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,7 +11,8 @@ plugins {
 android {
     namespace = "com.example.mw_antenatalcare"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    ndkVersion = "29.0.13599879"
+
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +28,7 @@ android {
         applicationId = "com.example.mw_antenatalcare"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
